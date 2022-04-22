04a51d5a3eca0a310377d06db982e3409224b3ed
Dan Kennedy <dannykennedy@email.com>
2022-04-22 18:08:28 +0100

    move constants to new file

---
+++ scripts/fakeConstants.ts
@@ -0,0 +1,46 @@
+const dummyWikipediaConcepts = [
+  {
+    title: "COVID-19",
+    link: "https://en.wikipedia.org/wiki/COVID-19",
+  },
+  {
+    title: "Donald Trump",
+    link: "https://en.wikipedia.org/wiki/Donald_Trump",
+  },
+  {
+    title: "Dog",
+    link: "https://en.wikipedia.org/wiki/Dog",
+  },
+  {
+    title: "Cat",
+    link: "https://en.wikipedia.org/wiki/Cat",
+  },
+  {
+    title: "Al Gore",
+    link: "https://en.wikipedia.org/wiki/Al_Gore",
+  },
+];
+
+// Months of the year
+const MONTHS = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+const LAST_WEEK_OF_YEAR_INDEX = 51;
+
+// 100 word dummy text
+const LOREM =
+  "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? ";
+
+export { dummyWikipediaConcepts, MONTHS, LAST_WEEK_OF_YEAR_INDEX, LOREM };
--- scripts/generateFakeData.ts
+++ scripts/generateFakeData.ts
@@ -1,6 +1,10 @@
 const fs = require("fs");
-
-const LAST_WEEK_OF_YEAR_INDEX = 51;
+const {
+  dummyWikipediaConcepts,
+  MONTHS,
+  LAST_WEEK_OF_YEAR_INDEX,
+  LOREM,
+} = require("./fakeConstants");
 
 /////////////// TYPES ///////////////
 
@@ -149,26 +153,6 @@ type ComparisonSet = {
 
 /////////////// CONSTANTS ///////////////
 
-// 100 word dummy text
-const LOREM =
-  "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? ";
-
-// Months of the year
-const MONTHS = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
-];
-
 const NUM_STOPS_IN_TIME_PERIOD: {
   [key in Granularity]: number;
 } = {
@@ -177,29 +161,6 @@ const NUM_STOPS_IN_TIME_PERIOD: {
   day: 365,
   year: 1,
 };
-
-const dummyWikipediaConcepts = [
-  {
-    title: "COVID-19",
-    link: "https://en.wikipedia.org/wiki/COVID-19",
-  },
-  {
-    title: "Donald Trump",
-    link: "https://en.wikipedia.org/wiki/Donald_Trump",
-  },
-  {
-    title: "Dog",
-    link: "https://en.wikipedia.org/wiki/Dog",
-  },
-  {
-    title: "Cat",
-    link: "https://en.wikipedia.org/wiki/Cat",
-  },
-  {
-    title: "Al Gore",
-    link: "https://en.wikipedia.org/wiki/Al_Gore",
-  },
-];
 
 /////////////// HELPER FUNCS ////////////////
 
