05b7fefd5998ef5b494c34e5f6306f286ae32fa2
Dan Kennedy <dannykennedy@email.com>
2022-08-16 15:24:15 +0100

    fix: consolidate comparisons list

--- scripts/modules/mergers.ts
+++ scripts/modules/mergers.ts
@@ -6,12 +6,44 @@ import {
 export const consolidateWikipediaConceptList = (
   bigList: WikipediaConcept[]
 ): WikipediaConcept[] => {
-  return bigList.slice(0, 5); // TODO
+  // We need to consolidate the list of concepts
+  // So there are no duplicates
+  const consolidatedConceptsDict = bigList.reduce((acc, concept) => {
+    // If the concept title includes the word "list", we ignore it
+    if (concept.title.toLowerCase().includes("list")) {
+      return acc;
+    }
+
+    if (acc[concept.link]) {
+      return {
+        ...acc,
+        [concept.link]: {
+          ...acc[concept.link],
+          score: acc[concept.link].score + concept.score,
+        },
+      };
+    } else {
+      return {
+        ...acc,
+        [concept.link]: concept,
+      };
+    }
+  }, {} as { [key: string]: WikipediaConcept });
+
+  const consolidatedConceptsArr = Object.values(consolidatedConceptsDict);
+
+  // Order list items by score
+  // So we get the most relevant items first
+  const sortedList = consolidatedConceptsArr.sort((a, b) => b.score - a.score);
+  return sortedList.slice(0, 5);
 };
 
 export const consolidateExampleList = (
   bigList: ExampleRecordComparison[],
   desiredLength: number
 ): ExampleRecordComparison[] => {
-  return bigList.slice(0, desiredLength); // TODO
+  // Order list items by score
+  // So we get the most relevant items first
+  const sortedList = bigList.sort((a, b) => b.score - a.score);
+  return sortedList.slice(0, desiredLength);
 };
