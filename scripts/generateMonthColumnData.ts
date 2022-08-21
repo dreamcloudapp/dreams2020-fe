8f6429e99b20a48978de492017719df2ca344e8e
Dan Kennedy <dannykennedy@email.com>
2022-08-21 09:58:48 +0100

    stub script for month column charts

---
+++ scripts/generateMonthColumnData.ts
@@ -0,0 +1,16 @@
+// const fs = require("fs");
+// const path = require("path");
+// const { isDotPath } = require("./modules/file-felpers");
+// const { getDifferenceInDays } = require("./modules/time-helpers");
+// import {
+//   DifferenceDictionary,
+//   DayRecord,
+//   NewsRecord,
+//   DifferenceRecord,
+//   DifferenceByGranularity,
+// } from "@kannydennedy/dreams-2020-types";
+// import { ColorTheme } from "./modules/theme";
+
+console.log("Generating month column data...");
+
+export {};
