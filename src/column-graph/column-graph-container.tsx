2c83e916791a3a015bf4b2e53fe985f2b295f4e6
Dan Kennedy <dannykennedy@email.com>
2022-10-21 17:45:23 +0700

    Update column-graph-container.tsx

--- src/column-graph/column-graph-container.tsx
+++ src/column-graph/column-graph-container.tsx
@@ -34,9 +34,9 @@ const renderTooltip = (d: ColumnGraphData) => {
   const monthName = monthNameFromIndex(month);
   return (
     <div>
-      <h3 style={{ textDecoration: "underline" }}>
+      <p style={{ textDecoration: "underline" }}>
         {monthName} 2020 dreams vs. {monthName} 2020 news
-      </h3>
+      </p>
       <p>
         <b>Average similarity: </b>
         {prettyNumber(avgSimilarity, 5)}
